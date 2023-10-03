81e0dd7bd1c23db1a18609d11f3806690ca2c50a
Faraz Jawed <farazjawed@Farazs-MacBook-Air.local>
2023-10-02 23:16:43 -0400

    done

--- mylib/query.py
+++ mylib/query.py
@@ -4,18 +4,14 @@ import sqlite3
 
 
 def query():
-    # Connect and query DB
-
-    print("Average of count_products:")
-    for row in cursor.fetchall():
-        print(row[0])
-
-    print("\nSum of count_products:")
-    for row in cursor.fetchall():
-        print(row[0])
-
-    # Close connection
-
+    """Query the database for the top 5 rows of the GroceryDB table"""
+    conn = sqlite3.connect("GroceryDB.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT avg (count_products) FROM GroceryDB")
+    print("Sum of count_products:", cursor.fetchall())
+    cursor.execute("SELECT sum (count_products) FROM GroceryDB")
+    print("Avg of count_products:", cursor.fetchall())
+    conn.close()
     return "Success"
 
 
